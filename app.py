6997fe9e959974b51e27235f420006f65cd0542a
Shriya Kar <shriyakar456@gmail.com>
2025-05-15 17:43:35 +0530

    Add feedback db

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def form():
     return render_template('form.html')
 
 if __name__ == '__main__':
-    app.run(debug=False, use_reloader=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
